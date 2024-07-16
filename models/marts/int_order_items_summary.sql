dadb0cbaa430547de84e8545f386e1897143f476
eddaouissam <eddaouissam@gmail.com>
2024-07-16 17:49:21 +0200

    Update DBT models (Staging & Marts), tests, packages, and DDL script for the SF environment

---
+++ models/marts/int_order_items_summary.sql
@@ -0,0 +1,10 @@
+--Create marts/int_order_items_summary.sql to aggregate info
+
+select 
+    order_key,
+    sum(extended_price) as gross_item_sales_amount,
+    sum(item_discount_amount) as item_discount_amount
+from
+    {{ ref('int_order_items') }}
+group by
+    order_key
---
+++ scripts/setup_snowflake_env.sql
@@ -0,0 +1,19 @@
+-- Setup The Demo Environment
+use role securityadmin;
+
+create role if not exists dbt_role;
+grant role dbt_role to user 'YOUR_USER_HERE';
+
+use role sysadmin;
+
+create or replace warehouse dbt_wh with warehouse_size='x-small';
+create database if not exists dbt_db;
+
+show grants on warehouse dbt_wh;
+
+grant usage on warehouse dbt_wh to role dbt_role;
+grant all on database dbt_db to role dbt_role;
+
+use role dbt_role;
+
+create schema if not exists dbt_db.dbt_schema;
